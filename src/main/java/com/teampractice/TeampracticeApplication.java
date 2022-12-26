0bff8ace6146188a0cc26c751819f01f25b6c81a
MohitSindhpure <mohitsindhpure1998@gmail.com>
2022-12-27 00:51:19 +0530

    mohit 1

---
+++ src/main/java/com/teampractice/TeampracticeApplication.java
@@ -0,0 +1,13 @@
+package com.teampractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeampracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeampracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teampractice/TeampracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teampractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeampracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
